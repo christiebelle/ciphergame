1b5ebd55351192ec68f24da3d95c714da4ec506e
Christiebelle <cslester@outlook.com>
2019-05-21 20:27:31 +0100

    indentation fixed

--- cipher.py
+++ cipher.py
@@ -1,3 +1,4 @@
+
 task = input('Do you want to encrypt or decrypt a message? ')
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -28,14 +29,14 @@ elif task == 'd':
             newMessage += character
     print('Your message is now: ' + newMessage)
 elif task == 'bf':
-        for i in range(len(alphabet)):
-            newMessage = ''
-            for character in message:
-                if character in alphabet:
-                    position = alphabet.find(character)
-                    newPosition = (position - i) % 26
-                    newCharacter = alphabet[newPosition]
-                    newMessage += newCharacter
-                else:
-                    newMessage += character
-            print('Key #%s: %s'%(i, newMessage))
+    for i in range(len(alphabet)):
+        newMessage = ''
+        for character in message:
+            if character in alphabet:
+                position = alphabet.find(character)
+                newPosition = (position - i) % 26
+                newCharacter = alphabet[newPosition]
+                newMessage += newCharacter
+            else:
+                newMessage += character
+        print('Key #%s: %s'%(i, newMessage))
