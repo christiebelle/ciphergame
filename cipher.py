fa464bc729f20980385e7f214326e3ebc467fdc9
Christiebelle <cslester@outlook.com>
2019-05-29 19:26:42 +0100

    code wrapped in loop for continuous running

--- cipher.py
+++ cipher.py
@@ -1,42 +1,47 @@
+task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+while task == 'e' or task == 'd' or task == 'bf':
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    newMessage = ''
 
-task = input('Do you want to encrypt or decrypt a message? ')
+    message = input('Please enter a message: ')
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-newMessage = ''
-
-message = input('Please enter a message: ')
-
-if task == 'e':
-    key = input('Please enter a key: ')
-    for character in message:
-        if character in alphabet:
-            position = alphabet.find(character)
-            newPosition = (position + key) % 26
-            newCharacter = alphabet[newPosition]
-            newMessage += newCharacter
-        else:
-            newMessage += character
-    print('Your message is now: ' + newMessage)
-elif task == 'd':
-    key = input('Please enter a key: ')
-    for character in message:
-        if character in alphabet:
-            position = alphabet.find(character)
-            newPosition = (position - key) % 26
-            newCharacter = alphabet[newPosition]
-            newMessage += newCharacter
-        else:
-            newMessage += character
-    print('Your message is now: ' + newMessage)
-elif task == 'bf':
-    for i in range(len(alphabet)):
-        newMessage = ''
+    if task == 'e':
+        key = input('Please enter a key - this will be a number between 1 and 26: ')
         for character in message:
             if character in alphabet:
                 position = alphabet.find(character)
-                newPosition = (position - i) % 26
+                newPosition = (position + key) % 26
                 newCharacter = alphabet[newPosition]
                 newMessage += newCharacter
             else:
                 newMessage += character
-        print('Key #%s: %s'%(i, newMessage))
+        print('Your message is now: ' + newMessage)
+        task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+    elif task == 'd':
+        key = input('Please enter a key: ')
+        for character in message:
+            if character in alphabet:
+                position = alphabet.find(character)
+                newPosition = (position - key) % 26
+                newCharacter = alphabet[newPosition]
+                newMessage += newCharacter
+            else:
+                newMessage += character
+        print('Your message is now: ' + newMessage)
+        task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+    elif task == 'bf':
+        for i in range(len(alphabet)):
+            newMessage = ''
+            for character in message:
+                if character in alphabet:
+                    position = alphabet.find(character)
+                    newPosition = (position - i) % 26
+                    newCharacter = alphabet[newPosition]
+                    newMessage += newCharacter
+                else:
+                    newMessage += character
+            print('Key #%s: %s'%(i, newMessage))
+        task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+    elif task == 'q':
+         print("Thank you! Goodbye!")
+         break
