71b3d1c64b8a7b6e46c4065e5d00548d207bd291
Christiebelle <cslester@outlook.com>
2019-09-02 18:39:06 +0100

    input changed to raw input to correct single quotes issue

--- cipher.py
+++ cipher.py
@@ -1,9 +1,9 @@
-task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+task = raw_input('Do you want to encrypt (e) or decrypt (d) a message? ')
 while task == 'e' or task == 'd' or task == 'bf':
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     newMessage = ''
 
-    message = input('Please enter a message: ')
+    message = raw_input('Please enter a message: ')
 
     if task == 'e':
         key = input('Please enter a key - this will be a number between 1 and 26: ')
@@ -16,7 +16,7 @@ while task == 'e' or task == 'd' or task == 'bf':
             else:
                 newMessage += character
         print('Your message is now: ' + newMessage)
-        task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+        task = raw_input('Do you want to encrypt (e) or decrypt (d) a message? ')
     elif task == 'd':
         key = input('Please enter a key: ')
         for character in message:
@@ -28,7 +28,7 @@ while task == 'e' or task == 'd' or task == 'bf':
             else:
                 newMessage += character
         print('Your message is now: ' + newMessage)
-        task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+        task = raw_input('Do you want to encrypt (e) or decrypt (d) a message? ')
     elif task == 'bf':
         for i in range(len(alphabet)):
             newMessage = ''
@@ -41,7 +41,7 @@ while task == 'e' or task == 'd' or task == 'bf':
                 else:
                     newMessage += character
             print('Key #%s: %s'%(i, newMessage))
-        task = input('Do you want to encrypt (e) or decrypt (d) a message? ')
+        task = raw_input('Do you want to encrypt (e) or decrypt (d) a message? ')
     elif task == 'q':
          print("Thank you! Goodbye!")
          break
